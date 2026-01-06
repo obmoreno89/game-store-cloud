18ed31fa6df62e88cccc18613ca20dee20a36873
Omar Barragan Moreno <omar@Omars-MacBook-Air.local>
2026-01-06 13:40:05 -0600

    FEATURE - secrea main.py para el retorno de la respuesta personalizada y para el inicio a la conexion a la base de datos

---
+++ ms-pay-method/app/core/main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
+from fastapi.security import HTTPBearer 
+from contextlib import asynccontextmanager
+from sqlmodel import SQLModel
+from db import engine
+from utils import generat_uuid
+
+security = HTTPBearer()
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+    
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    
+    yield
+    
+    print("Limpiando recurso")
+
+app = FastAPI(title="{URL}/game-store/v1/metodos/pagos", version= "1.0.0",
+    description="API para generar metodo de pago", swagger_ui_parameters={"operationsSorter": "method"},
+    lifespan=lifespan, 
+    openapi_url="/openapi.json",
+    docs_url="/docs")
+
+@app.exception_handler(HTTPException)
+async def custom_http_exeption_handler(request: Request, exc: HTTPException):
+    folio = generat_uuid()
+    if isinstance(exc.detail, dict):
+        return JSONResponse(
+            status_code=exc.status_code,
+            content=exc.detail,
+            headers=exc.headers
+        )
+        
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"folio": folio, "mensaje": str(exc.detail)},
+        headers=exc.headers
+    )
+    
+app.include_router(router)
+    
+
+    
---
+++ ms-pay-method/app/utils/uuid.py
@@ -0,0 +1,4 @@
+import uuid
+
+def generat_uuid():
+    return str(uuid.uuid4())
